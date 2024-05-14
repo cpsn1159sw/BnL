13aaa770ad5aa05f7cfd885dc49b774baa236d98
Thien <dinhthien1159@gmail.com>
2024-05-14 14:56:36 +0700

    user

--- modules/User/forgot.php
+++ modules/User/forgot.php
@@ -7,4 +7,48 @@
 $data = [
     'pageTitle' => 'BnL - Quên mật khẩu'
 ];
-layouts('header', $data);
+?>
+
+    <title>BnL - Quên mật khẩu</title>
+  <!-- Themefisher Icon font -->
+  <link rel="stylesheet" href="<?php echo _WEB_HOST_TEMPLATES ?>/plugins/themefisher-font/style.css">
+  <!-- bootstrap.min css -->
+  <link rel="stylesheet" href="<?php echo _WEB_HOST_TEMPLATES ?>/plugins/bootstrap/css/bootstrap.min.css">
+  
+  <!-- Animate css -->
+  <link rel="stylesheet" href="<?php echo _WEB_HOST_TEMPLATES ?>/plugins/animate/animate.css">
+  <!-- Slick Carousel -->
+  <link rel="stylesheet" href="<?php echo _WEB_HOST_TEMPLATES ?>/plugins/slick/slick.css">
+  <link rel="stylesheet" href="<?php echo _WEB_HOST_TEMPLATES ?>/plugins/slick/slick-theme.css">
+  
+  <!-- Main Stylesheet -->
+  <link rel="stylesheet" href="<?php echo _WEB_HOST_TEMPLATES ?>/css/style.css">
+
+<body id="body">
+
+    <section class="forget-password-page account">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 col-md-offset-3">
+                    <div class="block text-center">
+                    <a class="logo" href="index.html">
+                        <img src="images/logo.png" alt="">
+                    </a>
+                    <h2 class="text-center">Welcome Back</h2>
+                    <form class="text-left clearfix">
+                        <p>Please enter the email address for your account. A verification code will be sent to you. Once you have received the verification code, you will be able to choose a new password for your account.</p>
+                        <div class="form-group">
+                        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Account email address">
+                        </div>
+                        <div class="text-center">
+                        <button type="submit" class="btn btn-main text-center">Request password reset</button>
+                        </div>
+                    </form>
+                    <p class="mt-20"><a href="?module=user&action=login">Back to log in</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+</body>
--- modules/User/signin.php
+++ modules/User/signin.php
@@ -1,15 +1,14 @@
-
 <?php
 // Chặn truy cập hợp lệ
     if(!defined('_CODE')) {
         die('Access denied...');
     }
 $data = [
-    'pageTitle' => 'BnL - Đăng nhập'
+    'pageTitle' => 'BnL - Đăng ký'
 ];
 ?>
 
-    <title>BnL - Đăng nhập</title>
+    <title>BnL - Đăng ký</title>
 
   <!-- Themefisher Icon font -->
   <link rel="stylesheet" href="<?php echo _WEB_HOST_TEMPLATES ?>/plugins/themefisher-font/style.css">
@@ -25,8 +24,6 @@ $data = [
   <!-- Main Stylesheet -->
   <link rel="stylesheet" href="<?php echo _WEB_HOST_TEMPLATES ?>/css/style.css">
 
-</head>
-
 <body id="body">
 
   <section class="signin-page account">
@@ -35,28 +32,34 @@ $data = [
         <div class="col-md-6 col-md-offset-3">
           <div class="block text-center">
             <a class="logo" href="index.html">
-              <img src="<?php echo _WEB_HOST_TEMPLATES ?>/images/logo/avata.png" alt="" style="height: 20vh;">
+              <img src="images/logo.png" alt="">
             </a>
-            <h2 class="text-center">Welcome Back</h2>
-            <form class="text-left clearfix" action="index.html" >
+            <h2 class="text-center">Create Your Account</h2>
+            <form class="text-left clearfix" action="index.html">
+              <div class="form-group">
+                <input type="text" class="form-control"  placeholder="Họ và tên">
+              </div>
+              <div class="form-group">
+                <input type="text" class="form-control"  placeholder="Địa chỉ">
+              </div>
+              <div class="form-group">
+                <input type="text" class="form-control"  placeholder="Username">
+              </div>
               <div class="form-group">
                 <input type="email" class="form-control"  placeholder="Email">
               </div>
               <div class="form-group">
-                <input type="password" class="form-control" placeholder="Password">
-              </div>
-              <div class="form-group">
-                <p><a href="?module=user&action=forgot">forgot password</a></p>
+                <input type="password" class="form-control"  placeholder="Password">
               </div>
               <div class="text-center">
-                <button type="submit" class="btn btn-main text-center" >Login</button>
+                <button type="submit" class="btn btn-main text-center">Sign In</button>
               </div>
             </form>
-            <p class="mt-20">New in this site ?<a href="?module=user&action=signin"> Create New Account</a></p>
+            <p class="mt-20">Already hava an account ?<a href="?module=user&action=login"> Login</a></p>
           </div>
         </div>
       </div>
     </div>
   </section>
-
+  
 </body>
